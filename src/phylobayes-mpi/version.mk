ce51cd2a9b72f851d889677ee14c883d87a73c79
Trevor Cooper <tcooper@sdsc.edu>
2013-11-25 15:09:17 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/phylobayes-mpi/version.mk
+++ src/phylobayes-mpi/version.mk
@@ -1,3 +1,14 @@
-NAME    = phylobayes-mpi_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 1.4e
-RELEASE	= 1
+NAME               = phylobayes-mpi_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 1.4e
+RELEASE            = 1
+PKGROOT            = /opt/phylobayes
+
+SRC_SUBDIR         = phylobayes-mpi
+
+SOURCE_NAME        = phylobayes-mpi
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
