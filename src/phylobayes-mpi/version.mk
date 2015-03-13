7f7f1b7173a451c43362337d1b26f8ce7ada254b
Jerry Greenberg <jpg@comet-devel.local>
2015-03-13 14:05:02 -0700

    updated to v 1.5a, which has no license files

--- src/phylobayes-mpi/version.mk
+++ src/phylobayes-mpi/version.mk
@@ -9,16 +9,16 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-phylobayes-mpi_$(COMPILERNAME)_$(MPINAME)
-VERSION        = 1.4e
+VERSION        = 1.5a
 RELEASE        = 2
 PKGROOT        = /opt/phylobayes
 
 SRC_SUBDIR     = phylobayes-mpi
 
-SOURCE_NAME    = phylobayes-mpi
+SOURCE_NAME    = pb_mpi
 SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
-SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_PKG     = $(SOURCE_NAME)$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 TAR_GZ_PKGS    = $(SOURCE_PKG)
